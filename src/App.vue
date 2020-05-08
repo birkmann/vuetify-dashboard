2bd8e6c07cc14c8fd1b0bbaf1acc54ce84b109b0
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-05-08 12:23:07 +0200

    files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app>
-      <!-- -->
+    <Sidebar />
+
+    <v-app-bar app color="primary" dark>
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
       <v-list-item>
         <v-list-item-avatar>
           <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
@@ -11,38 +16,7 @@
           <v-list-item-title>John Leider</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
-
-      <v-divider></v-divider>
-    </v-navigation-drawer>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn color="pink" dark @click.stop="drawer = !drawer">Toggle</v-btn>
+      <v-btn color="black" dark @click.stop="drawer = !drawer">Toggle</v-btn>
     </v-app-bar>
 
     <v-content>
@@ -54,12 +28,14 @@
 <script lang="ts">
 import Vue from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import Sidebar from "./components/Sidebar.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    HelloWorld
+    HelloWorld,
+    Sidebar
   },
 
   data: () => ({
